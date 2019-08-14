33805ce434bafd78b4b5da344a7248070ab73f94
yaromyrshmid <yaromyr.shmid@gmail.com>
2019-08-14 21:35:24 +0300

    navbar component

--- my-app/src/Components/Navigation.js
+++ my-app/src/Components/Navigation.js
@@ -5,34 +5,40 @@ import { Navbar, Nav, Row, Col, Container } from 'react-bootstrap';
 import '../css/Navigation.css';
 
 
-function Navigation() {
-  return (
-    <Container className="navbar-container">
+export class Navigation extends React.Component {
+  render() {
+    return (
+      <Container className="navbar-container">
 
-      <Navbar className=" col-md-2">
-        
-        <Row> 
-          <Navbar.Brand href="#Screen1"><Logo /></Navbar.Brand>
-        </Row>
-        <Row>
-          <Col>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav>
-                <Scrollspy items={ ['Screen1', 'Screen2', 'Screen3', 'Screen4'] } currentClassName="is-current">
-                <li><a href="#Screen1">ісія</a></li>
-                  <li><a href="#Screen2">Наша місія</a></li>
-                  <li><a href="#Screen3">Послуги</a></li>
-                  <li><a href="#Screen4">Контакти</a></li>
-                </Scrollspy>
-                </Nav>
-              </Navbar.Collapse>
-          </Col>
-        </Row>
-      </Navbar>
-    </Container>
-    
-  );
+        <Navbar className=" col-md-2">
+          
+          <Row> 
+            <Navbar.Brand href="#Screen1"><Logo /></Navbar.Brand>
+          </Row>
+          <Row>
+            <Col>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav>
+                  <Scrollspy 
+                  items={ ['Screen1', 'Screen2', 'Screen3', 'Screen4'] } currentClassName="is-current"
+                  onUpdate={
+                    (el) => {
+                      console.log(el)
+                    }
+                  }
+                  >
+                  <li><a href="#Screen1">ісія</a></li>
+                    <li><a href="#Screen2">Наша місія</a></li>
+                    <li><a href="#Screen3">Послуги</a></li>
+                    <li><a href="#Screen4">Контакти</a></li>
+                  </Scrollspy>
+                  </Nav>
+                </Navbar.Collapse>
+            </Col>
+          </Row>
+        </Navbar>
+      </Container>
+    );
+  }
 }
-
-export default Navigation;
--- my-app/src/Components/Parallax.js
+++ my-app/src/Components/Parallax.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navigation from './Navigation';
+import { Navigation } from './Navigation';
 import Screen1 from './Screen1'; 
 import Screen2 from './Screen2';
 import Screen3 from './Screen3';
