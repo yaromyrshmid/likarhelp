bbc5342714ccce29cd9d06decd9dc2c5206d4efa
yaromyr.shmid <yshmid@ukrinsoft.net>
2021-11-03 21:09:28 +0200

    Changed price

--- src/data/priceTags.js
+++ src/data/priceTags.js
@@ -2,16 +2,11 @@ export default [
   {
     line1: "Виклик",
     line2: "медсестри",
-    price: 200,
-  },
-  {
-    line1: "Лікування",
-    line2: "вдома",
     price: 300,
   },
   {
     line1: "Консультація",
     line2: "вдома",
-    price: 200,
+    price: 300,
   },
 ];
